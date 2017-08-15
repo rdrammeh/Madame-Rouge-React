a8e19e4af69f59309266079a552271d80e5a9343
Sara Sulit <email": null>
2017-08-15 02:46:12 -0400

    recipe index

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Recipes from './recipe_index/Recipes';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render((<Recipes/>)
+, document.getElementById('root'));
 registerServiceWorker();
