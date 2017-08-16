8f23857f65905ad2fa635be0814e676d39420c45
Rakey Drammeh <rdrammeh@gmail.com>
2017-08-15 21:10:06 -0400

    Link items in menu

--- src/Admin/currentmenu.js
+++ src/Admin/currentmenu.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
+import { Link }  from 'react-router-dom';
+
 
 export default class AdminCurrentMenu extends Component{
   render() {
     return (
       <div>
         <ul>
-          {this.props.menus.map(item => <li key={item.id}>{item.name}</li>)}
+          {this.props.menus.map(item => <li key={item.id}><Link to={`/recipes/${item.id}`}>{item.name}</Link></li>)}
         </ul>
       </div>
     )
