a4ccf65da09da910aa38af85d0c0168101f0faab
Sara Sulit <email": null>
2017-08-15 22:10:48 -0400

    new recipe

--- src/index.js
+++ src/index.js
@@ -7,15 +7,20 @@ import Recipes from './recipe_index/Recipes';
 import Homepage from './Homepage/homepage'
 import Show from './recipeshow/show'
 import registerServiceWorker from './registerServiceWorker';
+import RecipeForm from './recipe_new/recipe_form';
 
 ReactDOM.render(
   (<BrowserRouter>
     <Switch>
       <Route exact path="/" component={Homepage}/>
-      <Route path="/recipes/:id" component={Show}/>
+      <Route path="/recipes/new" component={RecipeForm}/>
       <Route path="/recipes" component={Recipes}/>
+      <Route path="/recipes/:id" component={Show}/>
     </Switch>
   </BrowserRouter>
   ), document.getElementById('root'));
 
 registerServiceWorker();
+
+
+// ingredients: [{amount: [amounts, amount]}, measurement: []]
