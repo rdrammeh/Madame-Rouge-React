d4af06cd6152a2db4290b7072a43d68b9fd69f66
Sara Sulit <email": null>
2017-08-16 10:16:54 -0400

    Style Forms

--- src/recipe_new/recipe_form.js
+++ src/recipe_new/recipe_form.js
@@ -50,6 +50,7 @@ export default class RecipeForm extends Component {
     }
   render() {
     return (
+    <div className="container">
       <section id="recipe-box">
         <p id="recipe-box-title">Make a new recipe-form</p>
         <form id="recipe-form" onSubmit={this.createRecipe}>
@@ -57,22 +58,28 @@ export default class RecipeForm extends Component {
             <input ref="name" type="text" />
           </label>
           <br/>
-          <label> Dish Type:
-            <select ref="dish_type" name="DishType" className="browser-default">
-              <option value="appetizer">Appetizer</option>
-              <option value="salad">Salad</option>
-              <option value="main course">Main Course</option>
-              <option value="desserts">Desserts</option>
-            </select>
-          </label>
-          <br/>
-          <label> Difficulty:
-            <select ref="difficulty" name="rec" className="browser-default">
-              <option value="easy">Easy</option>
-              <option value="medium">Medium</option>
-              <option value="hard">Hard</option>
-            </select>
-          </label>
+          <div className="row">
+            <div className="col s6">
+              <label> Dish Type:
+                <select ref="dish_type" name="DishType" className="browser-default">
+                  <option value="appetizer">Appetizer</option>
+                  <option value="salad">Salad</option>
+                  <option value="main course">Main Course</option>
+                  <option value="desserts">Desserts</option>
+                </select>
+              </label>
+            </div>
+
+            <div className="col s6">
+              <label> Difficulty:
+                <select ref="difficulty" name="rec" className="browser-default">
+                  <option value="easy">Easy</option>
+                  <option value="medium">Medium</option>
+                  <option value="hard">Hard</option>
+                </select>
+              </label>
+            </div>
+          </div>
           <br/>
           <label> Directions:
             <textarea ref="directions" id="new-recipe" cols="30" rows="5" maxLength="140" name="recipe"></textarea>
@@ -96,31 +103,34 @@ export default class RecipeForm extends Component {
                 </label>
               </label>
             </div>
-          <button type="button" onClick={this.addIngredient}>Add More Ingredients</button>
-        <br/>
-            {this.state.ingredientsList.map( (id, i) => {
-              return (
-                <div key={i}>
-                  <label>Ingredient:
-                    <br/>
-                    <label>Amount:
-                        <input ref="amount" type="text"/>
+          <br/>
+              {this.state.ingredientsList.map( (id, i) => {
+                return (
+                  <div key={i}>
+                    <label>Ingredient:
+                      <br/>
+                      <label>Amount:
+                          <input ref="amount" type="text"/>
+                      </label>
+                      <label>Measurement:
+                        <input ref="measurement" type="text"/>
+                      </label>
+                      <label>Item:
+                        <input ref="item" type="text"/>
+                      </label>
                     </label>
-                    <label>Measurement:
-                      <input ref="measurement" type="text"/>
-                    </label>
-                    <label>Item:
-                      <input ref="item" type="text"/>
-                    </label>
-                  </label>
-                </div>)
-            })}
-
-          <label>
-            <input type="submit" value="Make a new recipe"/>
-          </label>
+                  </div>)
+              }
+            )
+          }
+          <button className="btn btn-primary" type="button" onClick={this.addIngredient}>Add More Ingredients</button>
+          <br/>
+          <br/>
+          <br/>
         </form>
       </section>
+      <input className="btn btn-primary" type="submit" value="Make a new recipe"/>
+    </div>
     );
   }
 }
